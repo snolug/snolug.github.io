8c939c60320b61f6da512ead0c4ee2a27eaddcfd
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-06-16 07:31:31 -0700

    i think it may work now

--- snolug.js
+++ snolug.js
@@ -1,12 +1,11 @@
-function getSunday() {
-  now = new Date();
-  var day = now.getDay(),
-      diff = now.getDate() - day + (day === 0 ? -7 : 1);
-  return new Date(now.setDate(diff));
+function getFirstSunday() {
+    now = new Date();
+    day = now.getDay();
+    diff = now.getDate() - day + (day === 0 ? -6 - 7 : 0 - 7);
+    return new Date(now.setDate(diff));
 }
 
-document.getElementById("firstsunday").textContent = getSunday();
-
+document.getElementById("firstsunday").textContent = getFirstSunday();
 
 window.onload = function () {
   document.getElementById("currentyear").textContent = new Date().getFullYear();
