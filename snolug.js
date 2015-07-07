42af08ed87a021fa656df6c4e6e9154f4c542cb6
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-07-06 20:13:15 -0700

    make the javascript vars more explanatory

--- snolug.js
+++ snolug.js
@@ -2,26 +2,26 @@
 // @param - integer: month - which month
 // @param - integer: year - which year
 function firstSunday(month, year) {
-    var d = new Date(year, month, 1, 0, 0, 0, 0);
+    var firstDayOfThisMonth = new Date(year, month, 1, 0, 0, 0, 0);
     var day = 0;
     // check if first of the month is a Sunday, if so set date to the second
-    if (d.getDay() === 0) {
+    if (firstDayOfThisMonth.getDay() === 0) {
         day = 1;
-        d = d.setDate(day);
-        d = new Date(d);
+        firstDayOfThisMonth = firstDayOfThisMonth.setDate(day);
+        firstDayOfThisMonth = new Date(firstDayOfThisMonth);
     }
     // check if first of the month is a Sunday, if so return the date, otherwise get to the Sunday following the first of the month
 
-    else if (d.getDay() != 1) {
-        day = 8 - (d.getDay());
-        d = d.setDate(day);
-        d = new Date(d);
+    else if (firstDayOfThisMonth.getDay() !== 1) {
+        day = 8 - (firstDayOfThisMonth.getDay());
+        firstDayOfThisMonth = firstDayOfThisMonth.setDate(day);
+        firstDayOfThisMonth = new Date(firstDayOfThisMonth);
     }
-    return d.shortFormat();
+    return firstDayOfThisMonth.shortFormat();
 }
 
 Date.prototype.shortFormat = function () {
-    return (this.getMonth() + 1) + "/" + this.getDate() + "/" + this.getFullYear();
+    return this.getFullYear() + "." + (this.getMonth() + 1) + "." + this.getDate();
 };
 
 var nowMonth = new Date().getMonth() + 1;
