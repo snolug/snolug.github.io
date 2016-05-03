6b9edd0876586e41f1f658ce270f49b6a76caafe
Ryan P.C. McQuen <ryan.q@linux.com>
2016-05-02 19:46:49 -0700

    Fix Wednesday calculation.
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- snolug.js
+++ snolug.js
@@ -1,5 +1,6 @@
 /*global window*/
 /*jslint browser:true, white:true*/
+
 (function () {
 
   'use strict';
@@ -9,16 +10,16 @@
   // @param - integer: year - which year
 
   var firstWednesday= function (month, year) {
-    var firstDayOfThisMonth = new Date(year, month, 1, 0, 0, 0, 0);
-    var day = 4;
+    var firstDayOfThisMonth = new Date(year, month, 5, 0, 0, 0, 0);
+    var day = 0;
     // check if first of the month is a Wednesday, if so set date to the second
-    if (firstDayOfThisMonth.getDay() === 4) {
-      day = 5;
+    if (firstDayOfThisMonth.getDay() === 0) {
+      day = 1;
       firstDayOfThisMonth = firstDayOfThisMonth.setDate(day);
       firstDayOfThisMonth = new Date(firstDayOfThisMonth);
     }
     // check if first of the month is a Wednesday, if so return the date, otherwise get to the Wednesday following the first of the month
-    else if (firstDayOfThisMonth.getDay() !== 5) {
+    else if (firstDayOfThisMonth.getDay() !== 1) {
       day = 8 - (firstDayOfThisMonth.getDay());
       firstDayOfThisMonth = firstDayOfThisMonth.setDate(day);
       firstDayOfThisMonth = new Date(firstDayOfThisMonth);
@@ -30,7 +31,7 @@
     return this.getFullYear() + "." + (this.getMonth() + 1) + "." + this.getDate();
   };
 
-  var nowMonth = new Date().getMonth();
+  var nowMonth = new Date().getMonth() + 1;
   var nowYear = new Date().getFullYear();
 
   window.addEventListener('load', function () {
